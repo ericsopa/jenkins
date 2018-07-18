0a840cd4f71e09fe46d7d0f7dc34d4733adaa5a4
root <root@master.(none)>
2018-07-18 01:56:56 +0000

    Added service start,stop, generate CSR

--- share/scripts/install-puppet-master.sh
+++ share/scripts/install-puppet-master.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 sudo yum -y install puppet-server
+wait
+sudo service puppetmaster start
+wait
+sudo service puppetmaster start
+wait
+timeout 15s sudo -u puppet puppet master --no-daemonize –verbose 
+wait
+puppet resource service puppetmaster ensure=running
+wait
+service puppetmaster status
+
