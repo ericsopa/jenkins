e472de387ba0cb3bfb7dc7e9c0f04b301eacaf30
Paul Ericson <paul.ericson@dynatrace.com>
2018-07-19 08:13:35 -0400

    Fixed site.pp

--- share/master/site.pp
+++ share/master/site.pp
@@ -1,9 +1,17 @@
-class { 'java' :
-  package => 'java-1.8.0-openjdk-devel',
-}
-tomcat::install { '/opt/tomcat':
-  source_url => 'https://archive.apache.org/dist/tomcat/tomcat-9/v9.0.0.M1/bin/apache-tomcat-9.0.0.M1.tar.gz'
-}
-tomcat::instance { 'default':
-   catalina_home => '/opt/tomcat',
+node agent {
+
+  class { 'java' :
+    package => 'java-1.8.0-openjdk-devel',
+  }
+
+  tomcat::install { '/opt/tomcat':
+    source_url => 'https://archive.apache.org/dist/tomcat/tomcat-9/v9.0.0.M1/bin/apache-tomcat-9.0.0.M1.tar.gz'
+  }
+
+  tomcat::instance { 'default':
+     catalina_home => '/opt/tomcat',
+  }
+
+  include jenkins
+
 }
