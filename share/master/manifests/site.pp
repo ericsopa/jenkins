6f2ba09e3c307ca2b9acda496fac268c516c86ff
vagrant <vagrant@puppet.localdomain>
2018-07-21 17:54:29 +0000

    Minor fixes

--- share/master/manifests/site.pp
+++ share/master/manifests/site.pp
@@ -2,11 +2,4 @@ node 'agent.localdomain' {
 
   include jenkins
 
-  file { '/etc/sysconfig/jenkins':
-    ensure => file,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0644',
-    source => 'puppet://modules/sysconfig/jenkins',
-  }
 }
